221ad028982774633dabb62f4af4af79edfaea73
Planeshifter <pburckhardt@outlook.com>
2015-07-22 08:18:40 +0200

    [INIT]

---
+++ test/fixtures/test.number.R
@@ -0,0 +1,16 @@
+options( digits = 16 )
+library( jsonlite )
+
+sigma = 1
+x = c( -5, -2.5, 0, 2.5, 5 )
+y = prayleigh( x, sigma )
+
+cat( y, sep = ",\n" )
+
+data = list(
+	sigma = sigma,
+	data = x,
+	expected = y
+)
+
+write( toJSON( data, digits = 16, auto_unbox = TRUE ), "./test/fixtures/number.json" )
